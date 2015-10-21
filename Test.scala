5856b8bb1e11279fdfebea413f821976c4ac0a26
Michael Pollmeier <michael@michaelpollmeier.com>
2015-10-21 17:35:44 +1300

    mareks version - need to ensure it's a product2

--- Test.scala
+++ Test.scala
@@ -7,26 +7,45 @@ object Main extends App {
   case class Label[A](name: String)
   case class LabelWithValue[A](label: Label[A], value: A)
 
-  var horribleGlobalState: Map[String, Any] = _
-  object GetLabelWithValue extends Poly1 {
-    implicit def caseLabel[A] = at[Label[A]] { label ⇒
-      // TODO: avoid the horrible global state - pass in the Map as a parameter
-      LabelWithValue(label, horribleGlobalState.get(label.name).asInstanceOf[A])
+  object combineLabelWithValue extends Poly2 {
+    implicit def atLabel[A, B <: HList] = at[Label[A], (Map[String, Any], B)] {
+      case (i, (map, res)) ⇒
+        (map, LabelWithValue(i, map.get(i.name).asInstanceOf[A]) :: res)
     }
   }
 
   val label1 = Label[Int]("a")
   val label2 = Label[String]("b")
   val labels = label1 :: label2 :: HNil
-  val labelsWithValues: LabelWithValue[Int] :: LabelWithValue[String] :: HNil = getValues(labels)
+  val labelsWithValues: LabelWithValue[Int] :: LabelWithValue[String] :: HNil = getValues(labels)._2
   println(labelsWithValues)
 
-  def getValues[L <: HList : *->*[Label]#λ, M <: HList](labels: L)(
-    implicit mapper: Mapper.Aux[GetLabelWithValue.type, L, M]) = {
+  // def getValues[L <: HList : *->*[Label]#λ, X, Z <: Product2[_,X]](labels: L)(
+  def getValues[L <: HList : *->*[Label]#λ, Z](labels: L)(
+    implicit folder: RightFolder.Aux[L, (Map[String, Any], HNil.type), combineLabelWithValue.type, Z]) = {
 
-    // simplified - imagine we get the content of the map from a database
-    horribleGlobalState = Map("a" -> 5, "b" -> "five")
-    // val a: Int =
-    labels map GetLabelWithValue
+    val state = Map("a" -> 5, "b" -> "five")
+    labels.foldRight((state, HNil))(combineLabelWithValue)
   }
 }
+
+// object Main extends App {
+//   case class Label[A](name: String)
+//   case class LabelWithValue[A](label: Label[A], value: A)
+
+//   object combine extends Poly2 {
+//     implicit def workS[A <: HList, B] = at[Label[B], (Map[String, Any], A)] {
+//       case (i, (map, res)) ⇒
+//         (map, LabelWithValue(i, map.get(i.name).asInstanceOf[B]) :: res)
+//     }
+//   }
+
+//   val state: Map[String, Any] = Map("a" -> 5, "b" -> "five")
+
+//   val label1 = Label[Int]("a")
+//   val label2 = Label[String]("b")
+
+//   val labels = label1 :: label2 :: HNil
+//   val mapped = labels.foldRight((state, HNil))(combine)._2
+//   println(mapped)
+// }
