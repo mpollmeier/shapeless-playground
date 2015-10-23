0d1ba2c7ab4cae80f7a13db6145a0041007d75a2
Michael Pollmeier <michael@michaelpollmeier.com>
2015-10-23 13:18:23 +1300

    simple version without intermediate step

--- Test.scala
+++ Test.scala
@@ -5,56 +5,26 @@ import shapeless.ops.tuple.IsComposite
 
 object Main extends App {
   case class Label[A](name: String)
-  case class LabelWithValue[A](label: Label[A], value: A)
 
   val label1 = Label[Int]("a")
   val labels = label1 :: HNil
 
-  object combineLabelWithValue extends Poly2 {
+  object GetValue extends Poly2 {
     implicit def atLabel[A, B <: HList] = at[Label[A], (B, Map[String, Any])] {
       case (label, (acc, values)) ⇒
-        (LabelWithValue(label, values(label.name).asInstanceOf[A]) :: acc, values)
+        (values(label.name).asInstanceOf[A] :: acc, values)
     }
   }
 
-  object GetLabelValue extends (LabelWithValue ~> Id) {
-    def apply[B](labelWithValue: LabelWithValue[B]) = labelWithValue.value
-  }
+  val values: Int :: HNil = getValues(labels)
 
-  val labelsWithValues: LabelWithValue[Int] :: HNil = getLabelWithValues(labels)
-  // manually mapping it works fine:
-  val valuesManual: Int :: HNil = labelsWithValues.map(GetLabelValue)
-
-  // using a second function with Mapper works fine:
-  val valuesSecondFn: Int :: HNil = getValues(labelsWithValues)
-
-  // error: could not find implicit value for parameter mapper: shapeless.ops.hlist.Mapper.Aux[Main.GetLabelValue.type,WithValues,Values]
-  // val valuesFull: Int :: HNil = getValuesFull(labels)
-
-
-  def getLabelWithValues[L <: HList, P, WithValues](labels: L)(
-    implicit folder: RightFolder.Aux[L, (HNil.type, Map[String, Any]), combineLabelWithValue.type, P],
-    ic: IsComposite.Aux[P, WithValues, _]
-  ): WithValues = {
+  def getValues[L <: HList, P, Values](labels: L)(
+    implicit folder: RightFolder.Aux[L, (HNil.type, Map[String, Any]), GetValue.type, P],
+    ic: IsComposite.Aux[P, Values, _]
+  ): Values = {
     val state = Map("a" -> 5, "b" -> "five")
-    val resultTuple = labels.foldRight((HNil, state))(combineLabelWithValue)
+    val resultTuple = labels.foldRight((HNil, state))(GetValue)
     ic.head(resultTuple)
   }
 
-  def getValues[WithValues <: HList, Values <: HList](withValues: WithValues)(
-    implicit mapper: Mapper.Aux[GetLabelValue.type, WithValues, Values]
-  ): Values = {
-    withValues.map(GetLabelValue)
-  }
-
-  def getValuesFull[L <: HList, P, WithValues <: HList, Values <: HList](labels: L)(
-    implicit folder: RightFolder.Aux[L, (HNil.type, Map[String, Any]), combineLabelWithValue.type, P],
-    ic: IsComposite.Aux[P, WithValues, _],
-    mapper: Mapper.Aux[GetLabelValue.type, WithValues, Values]
-  ): Values = {
-    val state = Map("a" -> 5, "b" -> "five")
-    val resultTuple = labels.foldRight((HNil, state))(combineLabelWithValue)
-    val withValues: WithValues = ic.head(resultTuple)
-    withValues.map(GetLabelValue)
-  }
 }
